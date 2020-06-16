04b83dc2276883c0094ae5a3178e147275dda456
Michael Braun <michaelbraunbass@gmail.com>
2020-06-16 14:07:10 -0600

    Update victim_ubuntu.tf

--- victim_ubuntu.tf
+++ victim_ubuntu.tf
@@ -52,7 +52,7 @@ resource "azurerm_public_ip" "vulnpublicip" {
     name                         = "${var.vulnvm-name}-public-ip"
     location                     = azurerm_resource_group.victim-network-rg.location
     resource_group_name          = azurerm_resource_group.victim-network-rg.name
-    allocation_method = "Dynamic"
+    allocation_method = "Static"
 }
 
 # Output the public ip of the gateway
