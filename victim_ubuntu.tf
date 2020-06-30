9b202b3a309ecce3f471080bbdfe45cc56aaabb1
Michael Braun <michaelbraunbass@gmail.com>
2020-06-30 09:20:12 -0600

    Update victim_ubuntu.tf

--- victim_ubuntu.tf
+++ victim_ubuntu.tf
@@ -91,6 +91,10 @@ resource "azurerm_virtual_machine" "main" {
   location              = azurerm_resource_group.victim-network-rg.location
   resource_group_name   = azurerm_resource_group.victim-network-rg.name
   network_interface_ids = [azurerm_network_interface.vuln-ubuntu.id]
+  depends_on = [
+    azurerm_network_interface_application_security_group_association.victim-linux-nsg-int
+  ]
+  
   vm_size               = "Standard_DS1_v2"
 
   delete_os_disk_on_termination = true
